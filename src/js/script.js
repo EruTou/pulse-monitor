5ca876f6e3a29e42a0bd049187ab1499a6e902b9
EruTou <e.nekei@mail.ru>
2023-03-14 03:02:56 +0700

    Updated 1.7

--- src/js/script.js
+++ src/js/script.js
@@ -34,4 +34,8 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal
+
+    $('[data-modal=consultation]').fadeOut;
 });
