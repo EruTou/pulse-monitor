3ec60c7136b490ddf31d0272f034acd6dba10bc2
EruTou <e.nekei@mail.ru>
2023-04-01 17:05:41 +0700

    updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,
-		// adaptiveHeight: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
 		responsive: [
 			{
-				breakpoint: 992,
+				breakpoint: 768,
 				settings: {
 					dots: true,
 					arrows: false
