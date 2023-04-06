22c36b5201459875c1c4d9e0e64a05b51abc39df
EruTou <e.nekei@mail.ru>
2023-04-07 04:26:07 +0700

    Update v2.0

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -8,25 +8,25 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+// $mail->SMTPDebug = 3;                                // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'rkzl.dkzl.info@mail.ru';                 // Наш логин
-$mail->Password = '5rhvA6ej2MEnUPztUvuF';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
+$mail->isSMTP();                                        // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                                 // Enable SMTP authentication
+$mail->Username = '';                 				    // Login
+$mail->Password = '';                           	    // Password (требуется специальный ключ для внешних приложений)
+$mail->SMTPSecure = 'ssl';                              // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                      // TCP port to connect to
 
-$mail->setFrom('rkzl.dkzl.info@mail.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('norotan387@mitigado.com');     // Add a recipient
-// $mail->addAddress('finogod444@dogemn.com');               // Name is optional
+$mail->setFrom('', 'Pulse');   							// From whom this letter 
+$mail->addAddress('');     								// Add a recipient
+// $mail->addAddress('finogod444@dogemn.com');          // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->isHTML(true);                                    // Set email format to HTML
 
 $mail->Subject = 'Данные';
 $mail->Body    = '
